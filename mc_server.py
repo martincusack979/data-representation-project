2b8b2281639f20ae0ca1732585d8b15b37a662c2
martincusack979 <cusackmartin@gmail.com>
2023-12-22 16:01:46 +0000

    dvd table

--- mc_server.py
+++ mc_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,5 +8,7 @@ def index():
 
 if __name__ == "__main__":
             app.run(debug = True)
+
+
 
 
