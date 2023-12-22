dad8885418bf373a04ba54d6d6f3d320c87023f3
martincusack979 <cusackmartin@gmail.com>
2023-12-22 16:25:04 +0000

    update flask server

--- mc_server.py
+++ mc_server.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return "Hello World"
+        return "blasht off!!"
+
+@app.route('/dvds')
+def getAll():
+        return "served by Get All()"
+
+@app.route('/dvds/<int:id>')
+def findById(id):
+        return "served by find by id with id" + str (id)
+
 
 if __name__ == "__main__":
             app.run(debug = True)
