ec254e6c9570710291c5dfadcbf8d83da570371b
martincusack979 <cusackmartin@gmail.com>
2023-12-23 17:44:20 +0000

    create dvd in flask

--- mc_server.py
+++ mc_server.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify, redirect, url_for, jsonify
+from flask import Flask, request, jsonify, redirect, abort
+
+
 
 app = Flask(__name__)
 
-DVDs = [
+dvds = [
    {"ID": 1, "Title": "The Shining", "Director": "Stanley Kubrick", "Year": "1980", "Price": 20},
    {"ID": 2, "Title": "Solaris", "Director": "Andrei Tarkovsky", "Year": "1972", "Price": 25},
    {"ID": 3, "Title": "Casino", "Director": "Martin Scorsese", "Year": "1995", "Price": 20},
@@ -14,26 +16,65 @@ DVDs = [
    {"ID": 9, "Title": "Blue Velvet", "Director": "David Lynch", "Year": "1986", "Price": 28}
    ]
 
+nextID = 10
+
 @app.route('/')
 def index():
         return "Good day to you, sir!"
 
+# get all dvds
 @app.route('/dvds')
 def getAll():
-        return jsonify(DVDs)
+        return jsonify(dvds)
 
+# find by ID
 @app.route('/dvds/<int:id>')
 def findById(id):
-        return "served by find by id with id" + str (id)
+        foundDvds = list(filter(lambda t:  t["ID"]== id, dvds))
+        if len (foundDvds) == 0:
+                return jsonify ({}), 204
+        return jsonify (foundDvds[0])
+        
+# create dvd
+# check that create works - curl -X POST -H "content-type:application/json" -d "{\"Title\":\"test\", 
+# \"Director\":\"some guy\", \"Year\":\"1999\", \"Price\":123}" http://127.0.0.1:5000/dvds
 
 @app.route('/dvds', methods =['POST'])
 def create():
-        return "served by Create"
+    global nextID
+    if not request.json:
+        abort(400)
 
+    dvd = {
+        "id" : nextID,
+        "Title" : request.json["Title"],
+        "Director" : request.json["Director"],
+        "Year" : request.json["Year"],
+        "Price" : request.json ["Price"]     
+        }
+    
+    dvds.append(dvd)
+    nextID += 1
+    return jsonify(dvd)
+
+# update dvds
 @app.route('/dvds', methods =['PUT'])
 def update():
-        return "served by Update with id"
+        foundDvds = list(filter(lambda t:  t["ID"]== id, dvds))
+        if len (foundDvds) == 0:
+                return jsonify ({}), 404
+        currentDvd = foundDvds[0]
+        if "Title" in request.json:
+                currentDvd["Title"] = request.json["Title"]
+        if "Director" in request.json:
+                currentDvd["Director"] = request.json["Director"]        
+        if "Year" in request.json:
+                currentDvd["Year"] = request.json["Year"]
+        if "Price" in request.json:
+                currentDvd["Price"] = request.json["Price"]
+        return jsonify(currentDvd)                
 
+# delete dvd
 @app.route('/dvds', methods =['DELETE'])
 def delete():
         return "served by Delete with id"
